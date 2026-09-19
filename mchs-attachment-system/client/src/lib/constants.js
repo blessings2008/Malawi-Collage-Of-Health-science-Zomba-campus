d351b7c7ce9953dc07cbff56562140faf5ac6425
blessings2008 <madalitsotambala2@gmail.com>
2026-09-19 09:19:01 +0200

    Restore existing programs and add new MCHS programs

--- mchs-attachment-system/client/src/lib/constants.js
+++ mchs-attachment-system/client/src/lib/constants.js
@@ -8,6 +8,8 @@ export const YEARS = [
 ];
 
 export const PROGRAMS = [
+  'Nursing and Midwifery',
+  'Certificate in Midwifery Technicians',
   'Certificate in Midwifery assistant',
   'Certificate in pharmacy',
   'Diploma in Community Healthy Nursing',
